8fae2fae0f492ac9bc05fcf0ff0744e012394b8f
eric3u <eric3u@outlook.com>
2019-02-27 21:48:24 +0800

    [Chore] 添加wiringPi 构建脚本

---
+++ tools/build_wiringPi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CURRENT_DIR=`pwd`
+SRC_ROOT=$CURRENT_DIR/../
+CROSS_COMPILE=arm-none-linux-gnueabihf-
+WIRING_NP_DIR=$SRC_ROOT/vendor/WiringNP
+
+# init submodule
+git submodule update --init
+
+cd $WIRING_NP_DIR
+
+cd wiringPi
+make uninstall
+make clean
+
+make CROSS_COMPILE=$CROSS_COMPILE static
+make install
+
+
